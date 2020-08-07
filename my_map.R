e5d0ebd6dd8fa06c930cca00864c80194867bfa8
asmae-toumi <atoumi.cu@gmail.com>
2020-08-07 17:57:03 -0400

    getting the skeleton ready for a r2d3 map

---
+++ my_map.R
@@ -0,0 +1,40 @@
+
+# Goals -------------------------------------------------------------------
+
+# Recreate a map similar to: http://bl.ocks.org/syntagmatic/623a3221d3e694f85967d83082fd4a77
+# Using: https://github.com/dreamRs/r2d3maps
+
+
+# Libraries ---------------------------------------------------------------
+library(r2d3maps)
+library(tidyverse)
+# to get county geometries
+library(tigris) 
+library(sf)
+library(rmapshaper) # to simplify the county geometries
+
+# Data --------------------------------------------------------------------
+
+# NYT COVID19 data
+nyt <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv") %>%
+  filter(date == "2020-08-01") %>% # just going to focus on one date
+  drop_na(fips) %>%
+  rename(GEOID = fips)
+
+# Joining NYT to US county geometries
+nyt_geo <- ms_simplify(tigris::counties(class = "sf")) %>%
+  select(GEOID, geometry) %>%
+  geo_join(nyt, by_sp = "GEOID", by_df = "GEOID")
+
+
+# Map ---------------------------------------------------------------------
+
+# When you run this, it will pull info from the js and css scripts
+r2d3map(
+  data = nyt_geo,
+  script = "./my_map.js" 
+) 
+
+
+
+
---
+++ my_map.js
@@ -0,0 +1,49 @@
+
+// D3 maps
+
+
+r2d3.onRender(function(json, svg, width, height, options) {
+  
+var color = d3.scaleThreshold()
+  .range(["#fde0dd","#fcc5c0","#fa9fb5","#f768a1","#dd3497","#ae017e","#7a0177","#49006a"])
+  .domain([250, 500, 750, 1000, 1250, 1500, 1750, 2000]);
+
+var tooltip = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden");
+
+  var projection = d3.geoAlbersUsa();
+
+  var path = d3.geoPath()
+      .projection(projection);
+
+  svg.attr("width", width)
+     .attr("height", height);
+
+  var states = topojson.feature(json, json.objects.states);
+
+  projection
+      .scale(1)
+      .translate([0, 0]);
+
+  var b = path.bounds(states),
+      s = 0.95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
+      t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+
+  projection
+      .scale(s)
+      .translate(t);
+
+  svg.append("path")
+      .datum(states)
+      .attr("class", "feature")
+      .attr("d", path);
+
+  svg.append("path")
+      .datum(topojson.mesh(json, json.objects.states, function(a, b) { return a !== b; }))
+      .attr("class", "mesh")
+      .attr("d", path);
+});
